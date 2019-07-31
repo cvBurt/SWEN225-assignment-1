9fbbcaccb20dacf47e6a41279326975ba407e2b6
cvBurt <39036924+cvBurt@users.noreply.github.com>
2019-07-31 15:09:06 +1200

    Add files via upload

--- Card.java
+++ Card.java
@@ -1,4 +1,18 @@
 
 public class Card {
+	private String type;
+	private String id;
+	
+	public Card(String type, String id) {
+		this.type = type;
+		this.id = id;
+	}
 
+	public String getType() {
+		return type;
+	}
+
+	public String getId() {
+		return id;
+	}
 }
