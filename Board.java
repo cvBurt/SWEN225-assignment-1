dbc1c46138807c27f8b0a02e77f7d016aa8eaa3d
Yumekaze <mattgyip@gmail.com>
2019-07-31 15:50:06 +1200

    Add files via upload

--- Board.java
+++ Board.java
@@ -11,142 +11,22 @@ public class Board {
 	}
 
 	public void constructBoard() {
-		board = new Cell[48][50];
-		String string = "";
-		Cell cell;
-		for (int column = 0; column < 48; column++) {
-			if ((column >= 0 && column <= 17) || column >= 30) {
-				string = " ";
-			}
-			else if (column == 20 || column == 27) {
-				string = "|";
-			}
-			else if (column > 20 && column < 27) {
-				string = "¯";
-			}
-			else {
-				string = "X";
-			}
-			cell = new Cell(string);
-			board[column][0] = cell;
-		}
-
-		for (int column = 0; column < 48; column++) {
-			if ((column >= 0 && column <= 17) || column >= 30 || (column > 20 && column < 27)) {
-				string = " ";
-			}
-			else if (column == 20 || column == 27) {
-				string = "|";
-			}
-			else {
-				string = "X";
-			}
-			cell = new Cell(string);
-			board[column][1] = cell;
-		}
-
-		for (int column = 0; column < 48; column++) {
-			if (column == 12 || column == 13 || column == 34 || column == 35 || (column > 20 && column < 27)) {
-				string = " ";
-			}
-			else if ((column > 0 && column < 11) ||(column > 36 && column < 47)) {
-				string = "¯";
-			}
-			else if (column == 0 || column == 11 || column == 20 || column == 27 || column == 36 || column == 47) {
-				string = "|";
-			}
-			else {
-				string = "X";
-			}
-			cell = new Cell(string);
-			board[column][2] = cell;
-		}
-
-		for (int column = 0; column < 48; column++) {
-			if ((column > 0 && column < 11) || (column > 36 && column < 47) || column == 12 || column == 13 || column == 34 || column == 35 || (column > 20 && column < 27)) {
-				string = " ";
-			}
-			else if (column == 0 || column == 11 || column == 20 || column == 27 || column == 36 || column == 47) {
-				string = "|";
-			}
-			else {
-				string = "X";
-			}
-			cell = new Cell(string);
-			board[column][3] = cell;
-		}
-
-		for (int column = 0; column < 48; column++) {
-			if (column == 0 || column == 11 || column == 16 || column == 31 || column == 36 || column == 47) {
-				string = "|";
-			}
-			else if ((column > 0 && column < 11) || (column > 36 && column < 47) || (column > 20 && column < 27)) {
-				string = " ";
-			}
-			else if (column > 16 & column < 31) {
-				string = "¯";	
-			} 
-			else {
-				string = "X";
-			}
-			cell = new Cell(string);
-			board[column][4] = cell;
-		}
-		
-		for (int column = 0; column < 48; column++) {
-			if (column == 0 || column == 11 || column == 16 || column == 31 || column == 36 || column == 47) {
-				string = "|";
-			}
-			else if ((column > 0 && column < 11) || (column > 36 && column < 47) || (column > 16 && column < 31)) {
-				string = " ";
-			}
-			else {
-				string = "X";
-			}
-			cell = new Cell(string);
-			board[column][5] = cell;
-			board[column][6] = cell;
-			board[column][7] = cell;
-			board[column][8] = cell;
-			board[column][9] = cell;
-		}
-		
-		for (int column = 0; column < 48; column++) {
-			if (column == 0 || column == 11 || column == 36 || column == 45) {
-				string = "|";
-			}
-			else if ((column > 0 && column < 11) || (column > 36 && column < 45) || (column >= 16 && column <= 31)) {
-				string = " ";
-			}
-			else if (column == 46 || column == 47) {
-				string = "¯";	
-			} 
-			else {
-				string = "X";
-			}
-			cell = new Cell(string);
-			board[column][10] = cell;
-		}
-		for (int column = 0; column < 48; column++) {
-			if (column == 0 || column == 11 || column == 36 || column == 45) {
-				string = "|";
-			}
-			else if ((column > 0 && column < 11) || (column > 36 && column < 48) || (column >= 16 && column <= 31)) {
-				string = " ";
-			}
-			else {
-				string = "X";
-			}
-			cell = new Cell(string);
-			board[column][11] = cell;
-		}
-
+		board = new Cell[24][25];
+		char[] blank = {'a',' ',' ',' ',' '};
+		char[] topLeftCorner = {'b','|', '¯', '|', ' '};
+		char[] topRightCorner = {'c','¯','|', '¯',' '};
+		char[] bottomLeftCorner = {'d','|', ' ', '¯', '¯'};
+		char[] bottomRightCorner = {'e',' ', '|', '¯', '¯'};
+		char[] rightEdge = {'f','|', ' ', '|', ' '};
+		char[] leftEdge = {'f',' ', '|', ' ', '|'};
+		char[] topEdge = {'g','¯', '¯', ' ', ' '};
+		char[] bottomEdge = {'g',' ', ' ', '¯', '¯'};
 	}
 
 	public void draw() {
-		for (int j = 0; j < 50; j++) {
+		for (int j = 0; j < 25; j++) {
 			System.out.print("\n");
-			for (int i = 0; i < 48; i++) {
+			for (int i = 0; i < 24; i++) {
 				Cell cell = board[i][j];
 				System.out.print(cell.getRoom());
 			}
--- Cell.java
+++ Cell.java
@@ -1,46 +1,85 @@
 
 public class Cell {
-	Cell northNeighbour;
-	Cell eastNeighbour;
-	Cell southNeighbour;
-	Cell westNeighbour;
+	Boolean northNeighbour;
+	Boolean eastNeighbour;
+	Boolean southNeighbour;
+	Boolean westNeighbour;
 	String room;
-	String draw;
-	
-	public Cell(Cell north, Cell east, Cell south, Cell west, String room) {
-		this.northNeighbour = north;
-		this.eastNeighbour = east;
-		this.southNeighbour = south;
-		this.westNeighbour = west;
+	char[] draw;
+
+	public Cell(String room, char[] draw) {
+		this.draw = draw;
+		if (draw[0] == 'a') {
+			this.northNeighbour = true;
+			this.eastNeighbour = true;
+			this.southNeighbour = true;
+			this.westNeighbour = true;
+		}
+		else if (draw[0] == 'b') {
+			this.northNeighbour = false;
+			this.eastNeighbour = true;
+			this.southNeighbour = true;
+			this.westNeighbour = false;
+		}
+		else if (draw[0] == 'c') {
+			this.northNeighbour = false;
+			this.eastNeighbour = false;
+			this.southNeighbour = true;
+			this.westNeighbour = true;
+		}
+		else if (draw[0] == 'd') {
+			this.northNeighbour = true;
+			this.eastNeighbour = true;
+			this.southNeighbour = false;
+			this.westNeighbour = false;
+		}
+		else if (draw[0] == 'e') {
+			this.northNeighbour = true;
+			this.eastNeighbour = false;
+			this.southNeighbour = false;
+			this.westNeighbour = true;
+		}
+		else if (draw[0] == 'f') {
+			this.northNeighbour = true;
+			this.eastNeighbour = false;
+			this.southNeighbour = true;
+			this.westNeighbour = false;
+		}
+		else {
+			this.northNeighbour = false;
+			this.eastNeighbour = true;
+			this.southNeighbour = false;
+			this.westNeighbour = true;
+		}
 		this.room = room;
 	}
-	
-	public Cell(String draw) {
-		this.draw = draw;
-	}
 
-	public Cell getNorthNeighbour() {
+	public Boolean getNorthNeighbour() {
 		return northNeighbour;
 	}
 
-	public Cell getEastNeighbour() {
+	public Boolean getEastNeighbour() {
 		return eastNeighbour;
 	}
 
-	public Cell getSouthNeighbour() {
+	public Boolean getSouthNeighbour() {
 		return southNeighbour;
 	}
 
-	public Cell getWestNeighbour() {
+	public Boolean getWestNeighbour() {
 		return westNeighbour;
 	}
-	
+
 	public void setRoom(String room) {
 		this.room = room;
 	}
-	
+
 	public String getRoom() {
 		return room;
 	}
-	
+
+	public char[] getDraw() {
+		return draw;
+	}
+
 }
